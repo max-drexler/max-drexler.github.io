ad366b6c6e03e5e23cc8e8b9677c478710747153
Max Drexler <mndrexler@gmail.com>
2023-06-30 00:40:27 -0500

    purple reformat

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,14 @@
-function toggleMenuIcon(obj) {
-    obj.classList.toggle('change');
+function showSections(){
+    $("#sections ul").css('display', 'flex');
 }
+
+$(document).ready(function(){
+    $("#section-selector ul li:not(.section-link)").on('click', function(){
+        $(this).addClass("selected-section");
+        $(this).siblings().removeClass("selected-section");
+        showSections();
+    });
+    $(window).scroll(function(){
+        showSections();
+    });
+});
